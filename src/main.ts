7e2ace77054cfd2aa02bc003b61e8d880d922442
ruzz <ruzz@arancino>
2021-05-15 12:31:54 +0200

    clean

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,8 @@ async function bootstrap() {
   //can inject service into custom validator
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  //app.set('views', __dirname + '/templates');
-  //app.set('view engine', 'jsx');
-  //app.engine('jsx', require('./core/template/engine').createEngine());
-
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
